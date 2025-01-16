4bfc52dd81bc7a7ffbee91e415f1f05ee72d08e9
Sabino <g_131n0@hotmail.com>
2025-01-16 17:51:49 -0300

    inclusao de formulario

--- src/Components/formularioContato/index.jsx
+++ src/Components/formularioContato/index.jsx
@@ -5,14 +5,29 @@ function Formulariocontato() {
     <>
       <section className="body-talkus">
         <div className="title-container">
-          <h2>FALE CONOSCO</h2>
+          <div className="teste">
+            <div className="container-content-text">
+              <h3>
+                Precisando de agilidade nos seus atos notariais?
+              </h3>
+              <h2>
+                Saiba na prática como podemos agilizar suas demandas e finalizar os processos em prazos surpreendentes.
+              </h2>
+              <h2>
+                Entre em contato com nossos profissionais e inicie o seu processo ainda hoje.
+              </h2>
+            </div>
+            <div className="container-arrow-textForm">
+              <span className="fa-regular fa-circle-right" />
+              <h4 style={{ color: "#fff", fontSize: "1.25rem" }}>
+                PREENCHA O FORMULÁRIO PARA SER ATENDIDO
+              </h4>
+            </div>
+          </div>
           <div className="border" style={{ width: "9%" }} />
         </div>
         <div className="container-form-info">
           <div className="container-form">
-            <h4 style={{ color: "#fff", fontSize: "1.25rem" }}>
-              PREENCHA O FORMULÁRIO ABAIXO PARA SER ATENDIDO
-            </h4>
             <form action="">
               <div className="content-name-email">
                 <div>
@@ -76,10 +91,10 @@ function Formulariocontato() {
                 <option value="">Inventario</option>
                 <option value="">Ata Notarial</option>
               </select>
+              <div className="container-button-send">
+                <button>ENVIAR MENSAGEM</button>
+              </div>
             </form>
-            <div className="container-button-send">
-              <button>ENVIAR MENSAGEM</button>
-            </div>
           </div>
         </div>
       </section>
